3628f0bd1af54b213283598236d6cbdba2eccdc4
Nathan Webb <nwrocketman@byui.edu>
2021-08-08 18:30:07 -0700

    Completed Section 8

---
+++ section8/assignment/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <active-user :username="user.name" :userage="user.age"></active-user>
+        <user-data @set-data="setUserData"></user-data>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+      return {
+          user: {
+              name: 'Nathan Webb',
+              age: 23
+          }
+      }
+  },
+  methods: {
+      setUserData(name, age) {
+          this.user = {
+              name: name,
+              age: +age
+          };
+      }
+  }
+}
+</script>
---
+++ section8/assignment/src/components/ActiveUser.vue
@@ -0,0 +1,12 @@
+<template>
+    <section>
+        <h2>{{ username }}</h2>
+        <h3>{{ userage }} Years</h3>
+    </section>
+</template>
+
+<script>
+export default {
+    props: ['username', 'userage']
+}
+</script>
