0d3fa5acb7558c3da14109bf6235f20f74e94d85
Nathan Webb <nwrocketman@byui.edu>
2021-08-08 23:07:25 -0700

    Completed Section 9

---
+++ section9/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <badge-list></badge-list>
+    <user-info
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></user-info>
+    <course-goals>
+      <template #default="slotProps">
+        <h2>{{ slotProps.item }}</h2>
+      </template>
+    </course-goals>
+  </div>
+</template>
+
+<script>
+import TheHeader from './components/TheHeader.vue';
+import UserInfo from './components/UserInfo.vue';
+import BadgeList from './components/BadgeList.vue';
+import CourseGoals from './components/CourseGoals.vue';
+
+export default {
+  components: {
+    TheHeader,
+    BadgeList,
+    UserInfo,
+    CourseGoals
+  },
+  data() {
+    return {
+      activeUser: {
+        name: 'Maximilian Schwarzmüller',
+        description: 'Site owner and admin',
+        role: 'admin',
+      },
+    };
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
