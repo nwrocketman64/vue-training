9bf7779f6a86dcb0b8c1d550822b4e2f6be1aafc
Nathan Webb <nwrocketman@byui.edu>
2021-09-10 10:11:21 -0700

    Complete Section 20

---
+++ section20/part2/src/components/DeleteUser.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+  <user-alert v-if="alertIsVisible" title="Delete the User?" @close="hideAlert">
+    <p>Do you want to continue with deleting a user?</p>
+  </user-alert>
+  <section>
+    <h2>Delete a User</h2>
+    <button @click="showAlert">Delete User</button>
+  </section>
+  </div>
+</template>
+
+<script>
+import UserAlert from './UserAlert.vue';
+import useAlert from '../hooks/alert.js';
+
+export default {
+  components: {
+    UserAlert,
+  },
+  setup() {
+    const [alertIsVisible, showAlert, hideAlert] = useAlert();
+
+    return {
+      alertIsVisible,
+      showAlert,
+      hideAlert
+    };
+  },
+};
+</script>
